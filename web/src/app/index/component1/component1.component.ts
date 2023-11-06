40f1a21b1ebeefeedf6896aca549610b50c96006
HHepan <792650995@qq.com>
2023-11-06 16:48:18 +0800

    0-2

--- web/src/app/index/component1/component1.component.ts
+++ web/src/app/index/component1/component1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-component1',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component1Component implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router,
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
   }
 
+  /**
+   * 关闭窗口
+   */
+  onClose() {
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
 }
--- web/src/common/dialog-entry/dialog-entry.module.ts
+++ web/src/common/dialog-entry/dialog-entry.module.ts
@@ -1,18 +1,13 @@
 import {NgModule} from '@angular/core';
-import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
-import {DialogEntryComponent} from './dialog-entry.component';
-import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {DialogEntryComponent} from './dialog-entry.component'
 
 @NgModule({
   declarations: [
     DialogEntryComponent
   ],
   imports: [
-    MatDialogModule,
-    CommonModule
-  ],
-  providers: [
-    {provide: MatDialogRef, useValue: {}},
+    MatDialogModule
   ],
   exports: [
     DialogEntryComponent
